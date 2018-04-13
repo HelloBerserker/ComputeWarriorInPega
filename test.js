e4f7926baa781e5a0eb7d125f9e24dcb0ac6a859
lijinfeng <lijinfeng520520@live.com>
2018-04-13 14:52:12 +0800

    删除远端的test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-var chars = ['A','U','O','Y','E','R',' ','G','M','P'];//Are you a programer
-var nums =[0,5,4,6,3,2,1,6,0,6,9,5,2,7,5,0,8,8,4,5];
- for(var i=0;i<nums.length; i++)
- 	document.write(chars[nums[i]]);
